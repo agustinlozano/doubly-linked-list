19b637c9b635b960c359825ef22c3b6406af7bc5
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-11-21 12:37:41 -0300

    Edit file: new features added to printList function

--- print.c
+++ print.c
@@ -1,20 +1,52 @@
 #include "dll.h"
 
 void printList(Node *head) {
-  Node *current = head;
-
   if (head == NULL) {
     printf("\nLa lista esta vacia. No hay nada que mostrar");
   
   } else {
-    int count = 0;
-    while (current != NULL) {
-      printf("\n%d. contiene: %d", count, current -> data);
+    int choice;
+    while (choice) {
+      printf("\n1. Imprimir de forma descentente");
+      printf("\n2. Imprimir de forma ascentente");
+      printf("\nSu respuesta: ");
+      scanf(" %d", &choice);
 
-      current = current -> next;
-      count++;
+      if (choice == 1 || choice == 2) {
+        (choice == 1 ? showDescending : showAscending)(head);
+        break;
+
+      } else {
+        printf("Ingresa una opcion valida\n");
+      }
     }
   }
   
   printf("\n\n");
+}
+
+
+void showAscending(Node *head) {
+  Node *current = head;
+  int count = 0;
+
+  while (current != NULL) {
+    printf("\n%d. contiene: %d", count, current -> data);
+
+    current = current -> next;
+    count++;
+  }
+}
+
+void showDescending(Node *head) {
+  Node *tail = getTail(head);
+  Node *current = tail;
+  int count = 0;
+
+  while (current != NULL) {
+    printf("\n%d. contiene: %d", count, current -> data);
+
+    current = current -> prev;
+    count++;
+  }
 }
