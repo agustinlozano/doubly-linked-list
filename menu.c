46556f39f2067c7e01895bcda16f51c54afb4053
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-11-21 11:58:05 -0300

    Edit file: remove an option

--- menu.c
+++ menu.c
@@ -5,8 +5,8 @@ int runMenu(void) {
 
   printf("\n\t1. Agregar un elemento al principio");
   printf("\n\t2. Agregar un elemento al final");
-  printf("\n\t3. Agregar un elemento seguido de otro");
-  printf("\n\t4. Imprimir la lista");
+  printf("\n\t3. Imprimir la lista");
+  printf("\n\t4. Eliminar elemento");
   printf("\n\t0. Salir");
   printf("\nSu eleccion: ");
   scanf(" %d", &choice);
