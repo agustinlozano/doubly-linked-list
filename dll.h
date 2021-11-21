55cb41c681eba14691399e4ad170c911d08070ae
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-11-21 12:39:30 -0300

    Update header

--- dll.h
+++ dll.h
@@ -5,9 +5,28 @@
 #include <stdlib.h>
 
 typedef struct node {
-  char *data;
+  int data;
   struct node *next;
   struct node *prev;
 } Node;
+
+/* menu */
+int runMenu(void);
+
+/* add nodes */
+void append(Node **head);
+void push(Node **head);
+  void addData(Node *n);
+  void addFirstNode(Node **head, Node *newNode);
+
+/* print */
+void printList(Node *head);
+  void showAscending(Node *head);
+  void showDescending(Node *head);
+
+/* delete nodes */
+void deleteNode(Node **head);
+  Node * getTail(Node *head);
+  Node * findNode(Node *head);
 
 #endif
